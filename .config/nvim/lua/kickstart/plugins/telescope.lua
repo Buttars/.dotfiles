df46c37d0a37b87e453770b1d15b3bcd8a9e5fb0
Landon Buttars <landon.buttars@wgu.edu>
2024-07-30 18:13:20 -0600

    feat: add git telescope keybinds

--- .config/nvim/lua/kickstart/plugins/telescope.lua
+++ .config/nvim/lua/kickstart/plugins/telescope.lua
@@ -85,6 +85,8 @@ return {
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>sc', '<cmd>Telescope git_commits<CR>', { desc = '[S]earch [C]ommits' })
+      vim.keymap.set('n', '<leader>gt', '<cmd>Telescope git_status<CR>', { desc = '[G]it status' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
