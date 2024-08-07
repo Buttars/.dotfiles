a2c9c848017a729accb1f364f9405486b0c30cb4
Buttars <17345308+Buttars@users.noreply.github.com>
2024-08-07 00:29:07 -0600

    feat(nvim): configure vimwiki plugin

--- .config/nvim/lua/custom/plugins/vimwiki.lua
+++ .config/nvim/lua/custom/plugins/vimwiki.lua
@@ -2,4 +2,13 @@ return {
   'vimwiki/vimwiki',
   event = 'VimEnter',
   lazy = true,
+  init = function()
+    vim.g.vimwiki_list = {
+      {
+        path = '~/vimwiki/',
+        syntax = 'markdown',
+        ext = '.md',
+      },
+    }
+  end,
 }
