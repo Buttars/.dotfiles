d969eb028f7de3a483b7a6a0eda2747550f66525
Buttars <17345308+Buttars@users.noreply.github.com>
2024-10-28 23:24:56 -0600

    fix: disable tokyonight to prevent conflict with cyberdream

--- .config/nvim/lua/custom/plugins/cyberdream.lua
+++ .config/nvim/lua/custom/plugins/cyberdream.lua
@@ -2,6 +2,10 @@ return {
   'scottmckendry/cyberdream.nvim',
   lazy = false,
   priority = 1000,
+  init = function()
+    vim.cmd.colorscheme 'cyberdream'
+    vim.cmd.hi 'Comment gui=none'
+  end,
   config = function()
     require('cyberdream').setup {
       theme = {
